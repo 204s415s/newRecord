cdc9e444734975f33b0f2784c3b5a467cc5c5c8d
204s415s <204s415s@stu.kobe-u.ac.jp>
2021-08-12 16:48:44 +0900

    change config

--- app/GoogleSheet.php
+++ app/GoogleSheet.php
@@ -18,7 +18,7 @@ class GoogleSheet {
         
         
         
-        $credentials_path = config('GOOGLE_CREDENTIALS');
+        $credentials_path = config('app.credentials');
         $client = new \Google_Client();
         $client->setScopes([[\Google_Service_Sheets::SPREADSHEETS]]);
         $client->setAuthConfig($credentials_path);
