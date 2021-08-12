dee5c0856d1ad5226f294a6001eb885e8a718a19
204s415s <204s415s@stu.kobe-u.ac.jp>
2021-08-12 17:25:37 +0900

    change GoogleSheet.php

--- app/GoogleSheet.php
+++ app/GoogleSheet.php
@@ -6,16 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class GoogleSheet {
     public static function instance() {
-        //herokuで動かしたいやつ
-        // $credential = env('GOOGLE_SHEET_CREDENTIAL');
-        // putenv('GOOGLE_SHEET_CREDENTIAL');
-        // $client = new \Google_Client();
-        // $client->useApplicationDefaultCredentials();
-        // //$client->setDeveloperKey("GOOGLE_CREDENTIALS");
-        // $client->setScopes([[\Google_Service_Sheets::SPREADSHEETS]]);
-        // //$client->setAuthConfig($credential);
-        // return new \Google_Service_Sheets($client);
-        
         $credentials_file = storage_path('app/json/lev-college-43ca656233f1.json');
         if(! file_exists($credentials_file)) {
             file_put_contents('credentials.json', getenv('GOOGLE_CREDENTIALS'));
@@ -26,13 +16,5 @@ class GoogleSheet {
         $client->setScopes([[\Google_Service_Sheets::SPREADSHEETS]]);
         $client->setAuthConfig($credentials_file);
         return new \Google_Service_Sheets($client);
-        
-        
-        //ローカルで動くやつ
-        // $credentials_path = storage_path('app/json/lev-college-43ca656233f1.json');
-        // $client = new \Google_Client();
-        // $client->setScopes([[\Google_Service_Sheets::SPREADSHEETS]]);
-        // $client->setAuthConfig($credentials_path);
-        // return new \Google_Service_Sheets($client);
     }
 }
