cfb1da8771f79def931ee8a1cc2b9099f63249f5
204s415s <204s415s@stu.kobe-u.ac.jp>
2021-08-12 14:26:02 +0900

    credential

--- app/GoogleSheet.php
+++ app/GoogleSheet.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class GoogleSheet {
     public static function instance() {
-        $credentials_path = storage_path('app/json/lev-college-43ca656233f1.json');
-        $credential = env('GOOGLE_SHEET_CREDENTIAL');
+        // $credentials_path = storage_path('app/json/lev-college-43ca656233f1.json');
+        // $credential = env('GOOGLE_SHEET_CREDENTIAL');
         $client = new \Google_Client();
+        $client->setDeveloperKey("GOOGLE_CREDENTIALS");
         $client->setScopes([[\Google_Service_Sheets::SPREADSHEETS]]);
         $client->setAuthConfig($credential);
         return new \Google_Service_Sheets($client);
